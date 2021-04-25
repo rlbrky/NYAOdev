fcc1d151163754173f034572ce024101ffdac11d
rlbrky <83189856+rlbrky@users.noreply.github.com>
2021-04-26 01:58:33 +0300

    Create eyleyici.java

---
+++ eyleyici.java
@@ -0,0 +1,8 @@
+public class Eyleyici {
+    public void SogutucuAc(){
+        System.out.println("Soğutucu açıldı.");
+    }
+    public void SogutucuKapa(){
+        System.out.println("Soğutucu kapatıldı.");
+    }
+}
